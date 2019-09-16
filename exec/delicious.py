8660a0519cda1e97733c1737740ebb5deeb28c5c
Hyungyo Seo <51025374+hyunbridge@users.noreply.github.com>
2019-09-16 18:01:19 +0900

    맛있는 메뉴 표시기능 추가

---
+++ exec/delicious.py
@@ -0,0 +1,17 @@
+# ██╗  ██╗██████╗ ███╗   ███╗███████╗ █████╗ ██╗
+# ██║  ██║██╔══██╗████╗ ████║██╔════╝██╔══██╗██║
+# ███████║██║  ██║██╔████╔██║█████╗  ███████║██║
+# ██╔══██║██║  ██║██║╚██╔╝██║██╔══╝  ██╔══██║██║
+# ██║  ██║██████╔╝██║ ╚═╝ ██║███████╗██║  ██║███████╗
+# ╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝
+# Copyright 2019, Hyungyo Seo
+# delicious.py - delicious.txt에서 키워드를 읽어와 list형으로 변환해주는 스크립트입니다.
+
+file = open("../data/delicious.txt", 'r', encoding="utf-8")
+menu = list()
+while True:
+    line = file.readline()
+    if not line: break
+    menu.append(line.replace("\n", ""))
+file.close()
+print(menu)
