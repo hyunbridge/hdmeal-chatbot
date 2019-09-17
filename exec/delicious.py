5e43b8736fd10050c3f98f5334b3d03c0958239e
hgyoseo <gohyungyo@outlook.com>
2019-09-17 20:45:38 +0900

    맛있는 음식 키워드 개선, delicious.py 기능 개선(중복 제거, 정렬 기능 추가)

--- exec/delicious.py
+++ exec/delicious.py
@@ -7,12 +7,21 @@
 # Copyright 2019, Hyungyo Seo
 # delicious.py - delicious.txt에서 키워드를 읽어와 list형으로 변환해주는 스크립트입니다.
 
-file = open("../data/delicious.txt", 'r', encoding="utf-8")
-menu = list()
-while True:
-    line = file.readline()
-    if not line:
-        break
-    menu.append(line.replace("\n", ""))
-file.close()
-print(menu)
+keyword = list()
+with open("../data/delicious.txt", 'r', encoding="utf-8") as file:
+    while True:
+        line = file.readline()
+        if not line:  # 내용이 없을 경우 반복문 탈출
+            break
+        keyword.append(line.replace("\n", ""))
+
+sort = sorted(list(set(keyword)))  # set 자료형으로 변환하여 중복제거 후 정렬
+
+text = str()
+for i in sort:
+    text = "%s\n%s" % (text, i)  # text 파일에 저장할 값 만들기
+text = text[1:]  # 맨 처음 줄바꿈 제거
+with open("../data/delicious.txt", 'w', encoding="utf-8") as file:
+    file.write(text)  # 파일에 쓰기
+
+print(sort)  # 코드에 반영할 수 있도록 리스트 출력
