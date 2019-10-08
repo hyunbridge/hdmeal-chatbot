8159c868f27c16041192fee7ff4428940afd66a5
Hyungyo Seo <51025374+hyunbridge@users.noreply.github.com>
2019-10-09 01:42:41 +0900

    기간 조회 알고리즘 개선

--- modules/getdata.py
+++ modules/getdata.py
@@ -7,9 +7,10 @@
 # Copyright 2019, Hyungyo Seo
 # modules/getdata.py - 급식, 시간표, 캐시정보를 가져오는 스크립트입니다.
 
+import datetime
+import json
 import os
 import urllib.request
-import json
 from collections import OrderedDict
 from modules import mealparser, calendarparser, wtemparser
 
@@ -91,16 +92,56 @@ def cal(year, month, date, debugging):
         with open('data/cache/Cal-%s-%s.json' % (year, month),
                   encoding="utf-8") as data_file:
             data = json.load(data_file, object_pairs_hook=OrderedDict)
-            json_data = data
     except FileNotFoundError:  # 파일 없을때
         if debugging:
             print("FileNotFound")
         return "일정이 없습니다."
 
     # 일정 있는지 확인
-    if date in json_data:
-        return json_data[date]
+    if date in data:
+        return data[date]
     return "일정이 없습니다."
+
+# 학사일정 가져오기 (다중)
+def cal_mass(start, end, debugging):
+
+    between_month = list()
+    between_date = list()
+    cal = list()
+
+    delta = (end - start).days  # 시작일과 종료일 사이의 일수를 구함
+
+    for i in range(delta + 1):  # 리스트에 시작일과 종료일 사이의 모든 달과 날짜를 담음
+        date = start + datetime.timedelta(days=i)
+        between_month.append((str(date.year).zfill(4), str(date.month).zfill(2)))
+        between_date.append((str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2)))
+
+    between_month = sorted(list(set(between_month)))  # List의 중복을 제거하고 정렬
+
+    for i in between_month:  # 대상월의 캐시가 있는지 확인, 없으면 만들기
+        if not os.path.isfile('data/cache/Cal-%s-%s.json' % (i[0], i[1])):
+            calendarparser.parse(i[0], i[1], debugging)
+
+    for i in between_date:
+        try:  # 파일 열기, JSON 데이터를 딕셔너리형으로 변환
+            with open('data/cache/Cal-%s-%s.json' % (i[0], i[1]),
+                      encoding="utf-8") as data_file:
+                data = json.load(data_file, object_pairs_hook=OrderedDict)
+        except FileNotFoundError:  # 파일 없을때
+            if debugging:
+                print("FileNotFound")
+            body = "일정이 없습니다."
+            cal.append((i[0], i[1], i[2], body))  # 년, 월, 일, 일정
+            continue  # 이후 구문 실행 않음
+
+        if i[2] in data:  # 일정이 있는지 확인
+            body = data[i[2]]
+        else:  # 없으면
+            body = "일정이 없습니다."
+
+        cal.append((i[0], i[1], i[2], body))  # 년, 월, 일, 일정
+
+    return cal
 
 
 # 한강 수온 가져오기
@@ -115,4 +156,4 @@ def wtemp(debugging):
 
 # 디버그
 if __name__ == "__main__":
-    print(cal(2019, 9, 30, True))
+    print(cal_mass(datetime.datetime(2019, 12, 1), datetime.datetime(2020, 2, 29), True))
