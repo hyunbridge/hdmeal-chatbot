e167c769f0837ad8fa820150aaf00ba0c86952d2
Hyungyo Seo <51025374+hyunbridge@users.noreply.github.com>
2019-10-28 19:19:03 +0900

    날씨 파서 부분 사소한 코드오류 수정

--- modules/weatherparser.py
+++ modules/weatherparser.py
@@ -26,26 +26,24 @@ def parse(debugging):
     data = xml.etree.ElementTree.fromstring(url.read().decode('utf-8')).findall('.//data')
 
     weather = dict()
-    loc = int()
-    for i in range(7):
+    for i in range(6):
         if data[i].find('hour').text == '9':  # 9시 찾기
-            if not i == 7:  # 다음날 9시가 아닐 경우
-                # 시간
-                weather['hour'] = data[i].find('hour').text
-                # 기온/최대 기온/최소 기온
-                weather['temp'] = data[i].find('temp').text
-                weather['temp_max'] = data[i].find('tmx').text
-                weather['temp_min'] = data[i].find('tmn').text
-                # 하늘 상태 -  1: 맑음 2: 구름조금 3: 구름많음 4: 흐림
-                weather['sky'] = data[i].find('sky').text
-                # 강수 형태 - 0: 없음 1: 비 2: 비&눈 3: 눈
-                weather['pty'] = data[i].find('pty').text
-                # 강수 확률
-                weather['pop'] = data[i].find('pop').text
-                # 습도
-                weather['reh'] = data[i].find('reh').text
-                loc = i
-                break
+            # 시간
+            weather['hour'] = data[i].find('hour').text
+            # 기온/최대 기온/최소 기온
+            weather['temp'] = data[i].find('temp').text
+            weather['temp_max'] = data[i].find('tmx').text
+            weather['temp_min'] = data[i].find('tmn').text
+            # 하늘 상태 -  1: 맑음 2: 구름조금 3: 구름많음 4: 흐림
+            weather['sky'] = data[i].find('sky').text
+            # 강수 형태 - 0: 없음 1: 비 2: 비&눈 3: 눈
+            weather['pty'] = data[i].find('pty').text
+            # 강수 확률
+            weather['pop'] = data[i].find('pop').text
+            # 습도
+            weather['reh'] = data[i].find('reh').text
+            loc = i
+            break
 
     if not weather:  # 날씨데이터 없을 경우(다음날 9시로 밀린 경우) 그 다음 데이터를 취함
         # 시간
