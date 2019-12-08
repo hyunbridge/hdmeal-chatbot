b1e9437604f87c9d7968c825f4d7b03429609eb8
hgyoseo <gohyungyo@outlook.com>
2019-12-08 21:46:20 +0900

    캐시 목록 조회시 캐시 만료까지 남은 시간도 알 수 있도록 개선, 시간표도 캐시하도록 개선

--- modules/TTParser.py
+++ modules/TTParser.py
@@ -14,6 +14,7 @@ import ast
 import datetime
 import base64
 from modules import log
+import os
 
 # 학교가 속한 지역과 학교의 이름을 정확히 입력
 # 검색 결과가 1개로 특정되도록 해주세요.
@@ -21,72 +22,133 @@ from modules import log
 school_region = "경기"
 school_name = "흥덕중학교"
 
+
 def parse(tt_grade, tt_class, year, month, date, req_id, debugging):
-    global sunday
+    global sunday, data_1, data_2
     part_code = str()
     tt_date = datetime.date(year, month, date)
     tt_grade = int(tt_grade)
     tt_class = int(tt_class)
 
-    log.info("[#%s] parse@modules/TTParser.py: Started Parsing Timetable(%s-%s, %s)" % (req_id, tt_grade, tt_class, tt_date))
+    log.info(
+        "[#%s] parse@modules/TTParser.py: Started Parsing Timetable(%s-%s, %s)" % (req_id, tt_grade, tt_class, tt_date))
 
-    try:
-        search_req = urllib.request.Request(
-            'http://comci.kr:4081/98372?92744l%s' % urlparse.quote(school_name.encode("EUC-KR")),
-            data=None,
-            headers={
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                              'Chrome/78.0.3904.70 Safari/537.36'
-            }
-        )
+    # 데이터 가져오기
+    def parse():
+        global part_code, data_1, data_2
 
-        search_url = urllib.request.urlopen(search_req)
-    except Exception as error:
+        # 학교명으로 검색해 학교코드 알아내기
+        try:
+            search_req = urllib.request.Request(
+                'http://comci.kr:4081/98372?92744l%s' % urlparse.quote(school_name.encode("EUC-KR")),
+                data=None,
+                headers={
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                  'Chrome/78.0.3904.70 Safari/537.36'
+                }
+            )
+
+            search_url = urllib.request.urlopen(search_req)
+        except Exception as error:
+            if debugging:
+                print(error)
+            log.err("[#%s] parse@modules/TTParser.py: Failed to Parse Timetable(%s-%s, %s)" % (
+                req_id, tt_grade, tt_class, tt_date))
+            return error
+
+        # 학교 검색결과 가져오기
+        school_list = ast.literal_eval(search_url.read().decode('utf-8').replace('\x00', ''))["학교검색"]
         if debugging:
-            print(error)
-        log.err("[#%s] parse@modules/TTParser.py: Failed to Parse Timetable(%s-%s, %s)" % (req_id, tt_grade, tt_class, tt_date))
-        return error
+            print(school_list)
 
-    # 학교 검색결과 가져오기
-    school_list = ast.literal_eval(search_url.read().decode('utf-8').replace('\x00', ''))["학교검색"]
-    if debugging:
-        print(school_list)
+        # 검색결과를 지역으로 구분하고 학교코드 가져오기
+        for i in school_list:
+            if i[1] == school_region:
+                part_code = i[3]
+                break
 
-    # 검색결과를 지역으로 구분하고 학교코드 가져오기
-    for i in school_list:
-        if i[1] == school_region:
-            part_code = i[3]
-            break
+        # 이번 주 시간표와 다음 주 시간표 가져오기
+        try:
+            fetch_req_1 = urllib.request.Request(
+                'http://comci.kr:4081/98372?' + base64.b64encode(bytes("34739_%s_0_1" % part_code, 'utf-8')).decode(
+                    "utf-8"),
+                data=None,
+                headers={
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                  'Chrome/78.0.3904.70 Safari/537.36'
+                }
+            )
 
-    try:
-        fetch_req_1 = urllib.request.Request(
-            'http://comci.kr:4081/98372?' + base64.b64encode(bytes("34739_%s_0_1" % part_code, 'utf-8')).decode("utf-8"),
-            data=None,
-            headers={
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                              'Chrome/78.0.3904.70 Safari/537.36'
-            }
-        )
+            fetch_req_2 = urllib.request.Request(
+                'http://comci.kr:4081/98372?' + base64.b64encode(bytes("34739_%s_0_2" % part_code, 'utf-8')).decode(
+                    "utf-8"),
+                data=None,
+                headers={
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                  'Chrome/78.0.3904.70 Safari/537.36'
+                }
+            )
 
-        fetch_req_2 = urllib.request.Request(
-            'http://comci.kr:4081/98372?' + base64.b64encode(bytes("34739_%s_0_2" % part_code, 'utf-8')).decode("utf-8"),
-            data=None,
-            headers={
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                              'Chrome/78.0.3904.70 Safari/537.36'
-            }
-        )
+            # 시간표 디코딩
+            url_1 = urllib.request.urlopen(fetch_req_1).read().decode('utf-8').replace('\x00', '')
+            url_2 = urllib.request.urlopen(fetch_req_2).read().decode('utf-8').replace('\x00', '')
+        except Exception as error:
+            if debugging:
+                print(error)
+            log.err("[#%s] parse@modules/TTParser.py: Failed to Parse Timetable(%s-%s, %s)" %
+                    (req_id, tt_grade, tt_class, tt_date))
+            return error
 
-        fetch_url_1 = urllib.request.urlopen(fetch_req_1)
-        fetch_url_2 = urllib.request.urlopen(fetch_req_2)
-    except Exception as error:
-        if debugging:
-            print(error)
-        log.err("[#%s] parse@modules/TTParser.py: Failed to Parse Timetable(%s-%s, %s)" % (req_id, tt_grade, tt_class, tt_date))
-        return error
+        # JSON 파싱
+        data_1 = json.loads(url_1)
+        data_2 = json.loads(url_2)
 
-    data_1 = json.loads(fetch_url_1.read().decode('utf-8').replace('\x00', ''))
-    data_2 = json.loads(fetch_url_2.read().decode('utf-8').replace('\x00', ''))
+        timestamp = int(datetime.datetime.now().timestamp())  # 타임스탬프 생성
+
+        # 캐시 만들기
+        with open('data/cache/TT.json', 'w', encoding="utf-8") as make_file:
+            json_cache = dict()
+            json_cache["1"] = dict()
+            json_cache["2"] = dict()
+            json_cache["Timestamp"] = timestamp
+            # 필요한 자료들만 선별에서 캐싱하기
+            json_cache["1"]["자료14"] = data_1["자료14"]
+            json_cache["1"]["자료81"] = data_1["자료81"]
+            json_cache["1"]["자료46"] = data_1["자료46"]
+            json_cache["1"]["자료92"] = data_1["자료92"]
+            json_cache["2"]["자료14"] = data_2["자료14"]
+            json_cache["2"]["자료81"] = data_2["자료81"]
+            json_cache["2"]["자료46"] = data_2["자료46"]
+            json_cache["2"]["자료92"] = data_2["자료92"]
+            json_cache["2"]["시작일"] = data_2["시작일"]
+            json.dump(json_cache, make_file, ensure_ascii=False, indent="\t")
+            print("File Created")
+
+    if os.path.isfile('data/cache/TT.json'):  # 캐시 있으면
+        try:
+            log.info("[#%s] parse@modules/TTParser.py: Read Data in Cache" % req_id)
+            with open('data/cache/TT.json', encoding="utf-8") as data_file:  # 캐시 읽기
+                global data_1, data_2
+                data = json.load(data_file)
+                data_1 = data["1"]
+                data_2 = data["2"]
+        except Exception:  # 캐시 읽을 수 없으면
+            try:
+                # 캐시 삭제
+                os.remove('data/cache/TT.json')
+            except Exception as error:
+                log.err("[#%s] parse@modules/TTParser.py: Failed to Delete Cache" % req_id)
+                return error
+            parse()  # 파싱
+        # 캐시 유효하면
+        if datetime.datetime.now() - datetime.datetime.fromtimestamp(data["Timestamp"]) < datetime.timedelta(hours=3):
+            log.info("[#%s] parse@modules/TTParser.py: Use Data in Cache" % req_id)
+        else:  # 캐시 무효하면
+            log.info("[#%s] parse@modules/TTParser.py: Cache Expired" % req_id)
+            parse()  # 파싱
+    else:  # 캐시 없으면
+        log.info("[#%s] parse@modules/TTParser.py: No Cache" % req_id)
+        parse()  # 파싱
 
     # 날짜비교 기준일(2번째 자료의 시작일) 파싱
     data_2_date = data_2["시작일"].split("-")
@@ -141,9 +203,11 @@ def parse(tt_grade, tt_class, year, month, date, req_id, debugging):
     elif tt_num > og_tt_num:
         return_data.append("[MSG]⭐연장수업이 있습니다. (%s교시 → %s교시)" % (og_tt_num, tt_num))
 
-    log.info("[#%s] parse@modules/TTParser.py: Succeeded to Parse Timetable(%s-%s, %s)" % (req_id, tt_grade, tt_class, tt_date))
+    log.info("[#%s] parse@modules/TTParser.py: Succeeded to Parse Timetable(%s-%s, %s)" % (
+    req_id, tt_grade, tt_class, tt_date))
 
     return return_data
+
 
 # 디버그
 if __name__ == "__main__":
--- modules/cache.py
+++ modules/cache.py
@@ -10,6 +10,8 @@
 import os
 from collections import OrderedDict
 from modules import log
+import datetime
+import json
 
 # 캐시 비우기
 def purge(req_id, debugging):
@@ -35,9 +37,27 @@ def get(req_id, debugging):
     return_data = str()
     for filename in filenames:
         ext = os.path.splitext(filename)[-1]
-        if ext == '.json':
+        if ext == '.json' and filename != "TT.json" and filename != "wtemp.json":  # 시간표와 수온 캐시 파일 숨김
             if debugging:
                 print(filename)
             return_data = "%s\n%s" % (return_data, filename.replace(".json", ""))
+    # 시간표 캐시 만료기한 조회
+    if "TT.json" in filenames:
+        with open('data/cache/TT.json', encoding="utf-8") as data:  # 캐시 읽기
+            timestamp = datetime.datetime.fromtimestamp(json.load(data)["Timestamp"])
+            if (datetime.datetime.now() - timestamp) < datetime.timedelta(hours=3):  # 캐시 만료됐는지 확인
+                time_left = int((datetime.timedelta(hours=3) - (datetime.datetime.now() - timestamp)).seconds / 60)
+                return_data = "%s\n시간표 캐시 만료까지 %s분 남음" % (return_data, time_left)
+            else:
+                return_data = "%s\n시간표 캐시 만료됨" % return_data
+    # 한강 수온 캐시 만료기한 조회
+    if "wtemp.json" in filenames:
+        with open('data/cache/wtemp.json', encoding="utf-8") as data:  # 캐시 읽기
+            timestamp = datetime.datetime.fromtimestamp(json.load(data)["timestamp"])
+            if (datetime.datetime.now() - timestamp) < datetime.timedelta(minutes=76):  # 캐시 만료됐는지 확인
+                time_left = int((datetime.timedelta(minutes=76) - (datetime.datetime.now() - timestamp)).seconds / 60)
+                return_data = "%s\n한강 수온 캐시 만료까지 %s분 남음" % (return_data, time_left)
+            else:
+                return_data = "%s\n한강 수온 캐시 만료됨" % return_data
     log.info("[#%s] get@modules/cache.py: Succeeded to Fetch Cache List" % req_id)
     return return_data
